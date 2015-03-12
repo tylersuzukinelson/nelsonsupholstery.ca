04794db4f75a9150c714324e840244bde004b098
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-03-11 20:38:29 -0700

    Added backbone for images controller

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,40 @@
+class ImagesController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :get_image, only: [:edit, :update, :destroy]
+  
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def get_image
+    @image = Image.find params[:id]
+  end
+
+  def image_params
+    params.require(:image).permit(:title, :description, :alt, :image)
+  end
+
+end
