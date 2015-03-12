ab3f39ed8d8ef6d6e4a478703b56d09592ba6732
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-03-11 18:14:40 -0700

    Added routes for sessions controller (fixed).

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create] do
+    delete :destroy, on: :collection
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
