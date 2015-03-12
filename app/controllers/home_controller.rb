8f946c6664e606ab036c0778247cf274e614a9fd
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-03-11 18:23:35 -0700

    Added home controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+
+  def index
+    @images = Image.order(created_at: :desc)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
   resources :sessions, only: [:new, :create] do
     delete :destroy, on: :collection
   end
