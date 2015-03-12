1eff323ed3524ede89f1da958c6363a82d3687b7
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-03-11 19:01:47 -0700

    Added menu to layout.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create] do
     delete :destroy, on: :collection
   end
+  resources :categories
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
