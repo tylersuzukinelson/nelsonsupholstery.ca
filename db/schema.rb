ec8cda75e6dc54fee75e9f04595140052371c9aa
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-05-01 13:18:17 -0700

    Added approved column to referrals table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423042626) do
+ActiveRecord::Schema.define(version: 20150501191140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20150423042626) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "approved"
   end
 
   create_table "site_contents", force: :cascade do |t|
